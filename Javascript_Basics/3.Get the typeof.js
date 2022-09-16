0e71f3c577c3ba0d727966050b417e66d2427919
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-09-16 15:55:28 +0530

    Basics

---
+++ Javascript_Basics/3.Get the typeof.js
@@ -0,0 +1,13 @@
+// Write a function that takes a value as argument
+// Return the type of the value
+/*function myFunction (a)
+{
+return typeof a;
+}*/
+
+
+var myNum1 = 4;
+var myNum2 = 98;
+function myFunction (myNum1 , myNum2) {
+    return(myNum1 + myNum2);
+}
