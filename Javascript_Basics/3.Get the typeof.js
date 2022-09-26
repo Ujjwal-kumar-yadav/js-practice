5de7614ea0bb7737f4a44042cb02f28d51825d18
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-09-26 20:05:45 +0530

    jschallenger

--- Javascript_Basics/3.Get the typeof.js
+++ Javascript_Basics/3.Get the typeof.js
@@ -1,13 +1,9 @@
 // Write a function that takes a value as argument
 // Return the type of the value
-/*function myFunction (a)
+
+
+function myFunction (a)
 {
 return typeof a;
-}*/
-
-
-var myNum1 = 4;
-var myNum2 = 98;
-function myFunction (myNum1 , myNum2) {
-    return(myNum1 + myNum2);
 }
+
---
+++ Javascript_object/Acc_obj_pro_one.js
@@ -0,0 +1,27 @@
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+
+function myFunction(obj) {
+    return obj.country;
+}
+
+
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key 'prop-2'
+// Tip: you might want to use the square brackets property accessor
+
+function myFunction(obj) {
+   
+    return obj['prop-2'];
+ }
+
+
+
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+
+function myFunction(obj, key) {
+   
+    return obj[key];
+ }
